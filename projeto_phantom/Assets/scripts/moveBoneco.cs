ccb1a1ae27fdcfbaf061d1ec4eaf1be0a49b8000
paulovitornovaes <paulonovaes@id.uff.br>
2021-01-13 18:22:26 -0300

    1.0
    adicionei sprites temporarios e fiz script de movimento

---
+++ projeto_phantom/Assets/scripts/moveBoneco.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveBoneco : MonoBehaviour
+{
+    public GameObject boneco;
+    public float vel = 2.9f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(vel * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(-vel * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, vel * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, - vel * Time.deltaTime, 0));
+        }
+    }
+}
