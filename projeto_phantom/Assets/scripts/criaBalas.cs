5fa231f20d1b13f90a4ab8470f5192f0267c062b
paulovitornovaes <paulonovaes@id.uff.br>
2021-01-14 18:53:55 -0300

    1.1
    coloquei script de criar balas e movimentar as balas

---
+++ projeto_phantom/Assets/scripts/criaBalas.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class criaBalas : MonoBehaviour
+{
+    public GameObject balas;
+    public GameObject cano;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Instantiate(balas, new Vector3(cano.transform.position.x, cano.transform.position.y, cano.transform.position.z), Quaternion.identity);
+        }
+    }
+}
---
+++ projeto_phantom/Assets/scripts/moveBalas.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveBalas : MonoBehaviour
+{
+    public float vel = 2.5f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector2(vel * Time.deltaTime,0));
+    }
+}
